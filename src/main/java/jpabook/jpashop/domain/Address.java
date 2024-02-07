0fcb4c1b3610eed0afc8c4b9d81c3c6014765d63
M1NS1K <rlaalstlr2001@gmail.com>
2024-02-07 16:23:41 +0900

    member-repository 생성 및 구현

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -1,10 +1,15 @@
 package jpabook.jpashop.domain;
 
 import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Embeddable //어딘가에 내장될 수 있다는 뜻
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED) //reflect proxy
 public class Address {
 
     private String city;
